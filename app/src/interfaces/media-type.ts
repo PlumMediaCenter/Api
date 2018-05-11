49a1c685eb0c3b430a0a1efc0059eb053b9eb113
Bronley Plumb <bronley@gmail.com>
2018-05-11 06:55:53 -0400

    Removed app code.

--- app/src/interfaces/media-type.ts
+++
@@ -1,8 +0,0 @@
-import { MediaTypeId } from "../enums/media-type-id";
-
-export interface MediaType {
-    id: MediaTypeId;
-    name: string;
-}
-
-
--- app/src/interfaces/source.ts
+++
@@ -1,9 +0,0 @@
-import { MediaTypeId } from "../enums/media-type-id";
-
-export interface Source {
-    id: number;
-    folderPath: string;
-    mediaTypeId: MediaTypeId;
-}
-
-
